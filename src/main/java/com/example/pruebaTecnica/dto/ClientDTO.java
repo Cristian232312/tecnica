b1b1d0af414ba65d3daa70eff0a6ec88c1c0a8c5
Cristian232312 <cadenacristian180@gmail.com>
2025-10-01 22:49:22 -0500

    se creó la entidad Client, y el DTO de Client

---
+++ src/main/java/com/example/pruebaTecnica/dto/ClientDTO.java
@@ -0,0 +1,79 @@
+package com.example.pruebaTecnica.dto;
+
+import java.time.LocalDate;
+
+public class ClientDTO {
+
+    private String identificationType;
+    private String identificationNumber;
+    private String name;
+    private String lastName;
+    private String email;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfCreate;
+    private LocalDate dateOfUpdate;
+
+    public String getIdentificationType() {
+        return identificationType;
+    }
+
+    public void setIdentificationType(String identificationType) {
+        this.identificationType = identificationType;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfCreate() {
+        return dateOfCreate;
+    }
+
+    public void setDateOfCreate(LocalDate dateOfCreate) {
+        this.dateOfCreate = dateOfCreate;
+    }
+
+    public LocalDate getDateOfUpdate() {
+        return dateOfUpdate;
+    }
+
+    public void setDateOfUpdate(LocalDate dateOfUpdate) {
+        this.dateOfUpdate = dateOfUpdate;
+    }
+}
---
+++ src/main/java/com/example/pruebaTecnica/entity/Client.java
@@ -0,0 +1,109 @@
+package com.example.pruebaTecnica.entity;
+
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "client")
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String identificationType;
+    private String identificationNumber;
+    private String name;
+    private String lastName;
+    private String email;
+    private LocalDate dateOfBirth;
+    private LocalDate dateOfCreate;
+    private LocalDate dateOfUpdate;
+
+    public Client(Long id, String identificationType, String identificationNumber, String name, String lastName, String email, LocalDate dateOfBirth, LocalDate dateOfCreate, LocalDate dateOfUpdate) {
+        this.id = id;
+        this.identificationType = identificationType;
+        this.identificationNumber = identificationNumber;
+        this.name = name;
+        this.lastName = lastName;
+        this.email = email;
+        this.dateOfBirth = dateOfBirth;
+        this.dateOfCreate = dateOfCreate;
+        this.dateOfUpdate = dateOfUpdate;
+    }
+
+    public Client() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getIdentificationType() {
+        return identificationType;
+    }
+
+    public void setIdentificationType(String identificationType) {
+        this.identificationType = identificationType;
+    }
+
+    public String getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(String identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(LocalDate dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public LocalDate getDateOfCreate() {
+        return dateOfCreate;
+    }
+
+    public void setDateOfCreate(LocalDate dateOfCreate) {
+        this.dateOfCreate = dateOfCreate;
+    }
+
+    public LocalDate getDateOfUpdate() {
+        return dateOfUpdate;
+    }
+
+    public void setDateOfUpdate(LocalDate dateOfUpdate) {
+        this.dateOfUpdate = dateOfUpdate;
+    }
+}
