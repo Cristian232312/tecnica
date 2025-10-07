4cb2174276a617a8cde61b70d60a6a784f0447d3
Cristian232312 <cadenacristian180@gmail.com>
2025-10-07 17:20:51 -0500

    Se agrego la anotacion @Repository en las interfaces de repositorio

--- src/main/java/com/example/pruebaTecnica/repository/ClientRepository.java
+++ src/main/java/com/example/pruebaTecnica/repository/ClientRepository.java
@@ -2,7 +2,9 @@ package com.example.pruebaTecnica.repository;
 
 import com.example.pruebaTecnica.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ClientRepository extends JpaRepository<Client, Long> {
     boolean existsByIdentificationNumber(String identificationNumber);
 }
